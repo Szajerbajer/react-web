bfc656c57acca9da2c16b39887e8b215475788a3
Marta Szajer <szajer.bajer@gmail.com>
2020-10-21 21:30:14 +0200

    add pages

--- web_bv/src/layouts/Blog.js
+++ web_bv/src/layouts/Blog.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import ProductPage from "../pages/ProductPage";
+import ContactPage from "../pages/ContactPage";
+import AdminPage from "../pages/AdminPage";
+import ErrorPage from "../pages/ErrorPage";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={HomePage} />;
+        <Route path="/product" exact component={ProductPage} />
+        <Route path="/contact" exact component={ContactPage} />
+        <Route path="/admin" exact component={AdminPage} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </>
+  );
 };
 
 export default Blog;
