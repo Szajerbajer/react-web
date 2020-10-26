4310c9640090f478d61f7bdcf25cac43291275db
Marta Szajer <szajer.bajer@gmail.com>
2020-10-26 20:18:20 +0100

    add admin page

--- web_bv/src/layouts/Blog.js
+++ web_bv/src/layouts/Blog.js
@@ -5,6 +5,7 @@ import ProductPage from "../pages/ProductPage";
 import ContactPage from "../pages/ContactPage";
 import AdminPage from "../pages/AdminPage";
 import ErrorPage from "../pages/ErrorPage";
+import LoginPage from "../pages/LoginPage"
 
 const Blog = () => {
   return (
@@ -14,6 +15,7 @@ const Blog = () => {
         <Route path="/product" exact component={ProductPage} />
         <Route path="/contact" exact component={ContactPage} />
         <Route path="/admin" exact component={AdminPage} />
+        <Route path="/login" exact component={LoginPage} />
         <Route component={ErrorPage} />
       </Switch>
     </>
--- web_bv/src/pages/AdminPage.js
+++ web_bv/src/pages/AdminPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import {Route, Redirect} from "react-router-dom";
+
+const permission = false;
 
 const AdminPage = () => {
-  return <div></div>;
+  return <Route render={()=>(
+    permission ? 
+    (<h3>Panel admina - dzień dobry </h3>) : (<Redirect to="/login" />)
+  )}/>;
 };
 
 export default AdminPage;
