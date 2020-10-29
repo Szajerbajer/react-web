c50a1c88c62c7b27b90fdd288c5e91580e2fc996
Marta Szajer <szajer.bajer@gmail.com>
2020-10-29 21:26:31 +0100

    add footer to learn match

--- web_bv/src/layouts/Footer.js
+++ web_bv/src/layouts/Footer.js
@@ -1,6 +1,28 @@
 import React from "react";
+import {Route} from 'react-router-dom';
+import ProductListPage from "../pages/ProductListPage";
+import "../styles/Footer.css"
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+  <div>
+    <h2>Footer</h2>
+    <Route path="/" exact render={() => {
+      return (
+        <p>Jesteś na stronie <span>głównej</span></p>
+      )
+    }}/>
+    <Route path="/:page" exact render={(props) => {
+      return (
+      <p>Jesteś na stronie <span>{props.match.params.page}</span></p>
+      )
+    }}/>
+    <Route path="/:page/:idProduct" exact render={(props) => {
+      return (
+      <p>Jesteś na stronie <span>{props.match.params.page}</span></p>
+      )
+    }}/>
+    </div>)
 };
 
 export default Footer;
