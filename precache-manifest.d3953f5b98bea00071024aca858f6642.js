de04861fd61426e3e22e38c79d419494c44830c8
Marta Szajer <szajer.bajer@gmail.com>
2020-11-01 17:20:28 +0100

    Updates

---
+++ precache-manifest.d3953f5b98bea00071024aca858f6642.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c057b52740e27b2d846144c9d63534",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "ee3067f36c0cbbb49040",
+    "url": "/react-web/static/css/main.00cbc1c4.chunk.css"
+  },
+  {
+    "revision": "081a35afb89d14e2a56b",
+    "url": "/react-web/static/js/2.07defb49.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-web/static/js/2.07defb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3067f36c0cbbb49040",
+    "url": "/react-web/static/js/main.a7175e53.chunk.js"
+  },
+  {
+    "revision": "f5faf1248e0aca912fe7",
+    "url": "/react-web/static/js/runtime-main.2ddeab9f.js"
+  },
+  {
+    "revision": "495ec5afc3d71852933eb3d1d88b434b",
+    "url": "/react-web/static/media/beach1.495ec5af.jpg"
+  },
+  {
+    "revision": "520b5a08fa8a85f97b4a29221f793b40",
+    "url": "/react-web/static/media/beach2.520b5a08.jpg"
+  },
+  {
+    "revision": "8ac960cac61b40386b93b84ef59f76d3",
+    "url": "/react-web/static/media/beach3.8ac960ca.jpg"
+  }
+]);
